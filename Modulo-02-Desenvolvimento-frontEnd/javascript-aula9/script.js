29dc37c32eed47cc6da44eba1baeb952c35bca4a
devdenisviana <denisviana@gmail.com>
2025-04-30 07:59:28 -0300

    uso de objetos e funções prontas

---
+++ Modulo-02-Desenvolvimento-frontEnd/javascript-aula9/script.js
@@ -0,0 +1,22 @@
+// let pessoa = {
+//     nome: "Lucas",
+//     idade: 25,
+//     cidade: "São Paulo",
+//     cursos: ["JavaScript", "HTML", "CSS"],
+// }
+
+// console.log(pessoa.nome); // Acessando a propriedade "nome" do objeto pessoa
+// console.log(pessoa.idade); // Acessando a propriedade "idade" do objeto pessoa      
+// console.log(pessoa.cidade); // Acessando a propriedade "cidade" do objeto pessoa
+// console.log(pessoa.cursos); // Acessando a propriedade "cursos" do objeto pessoa
+
+//uso de objetos:
+localStorage.setItem("nome", "Denis");
+
+// setTimeout(() => {
+//     alert("Olá pessoal")
+// }, 3000);
+
+setInterval(() =>  {
+    console.log("Olá pessoal")
+}, 1000);
