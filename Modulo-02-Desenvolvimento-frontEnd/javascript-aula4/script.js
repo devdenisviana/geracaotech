c932f7d22bb941c25a5c03838c659dd9e0859ef6
devdenisviana <denisviana@gmail.com>
2025-05-04 14:49:58 -0300

    prática de javascript

---
+++ Modulo-02-Desenvolvimento-frontEnd/javascript-aula4/script.js
@@ -0,0 +1,11 @@
+let numero1 = Number(prompt("Digite um número:"));
+let numero2 = Number(prompt("Digite outro número:"));
+
+if (numero1 > 5) {
+    console.log("O primeiro número é maior que 5.");
+} else if (numero1 < 5) {
+    console.log("O primeiro número é menor que 5.");
+} else {
+    console.log("O primeiro número é igual a 5.");
+}
+
