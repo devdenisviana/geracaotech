32f7cf49e03cf6d4a363043e3f463f4cdadf538a
devdenisviana <denisviana@gmail.com>
2025-05-05 08:52:19 -0300

    uso de switch case

---
+++ Modulo-02-Desenvolvimento-frontEnd/javascript-aula5/script.js
@@ -0,0 +1,18 @@
+let mode = "green";
+
+switch(mode) {
+    case "dark":
+        document.querySelector("body").style.backgroundColor = "#222222";
+        break;
+
+    case "red":
+        document.querySelector("body").style.backgroundColor = "#FF0000";   
+        break;
+
+        case "blue":
+        document.querySelector("body").style.backgroundColor = "#0000FF";   
+        break;
+
+        default:
+        document.querySelector("body").style.backgroundColor = "#FF00FF";
+}
