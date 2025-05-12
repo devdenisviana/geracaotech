2d44835d846691ffb20ee630544a9ee70bae066a
devdenisviana <denisviana@gmail.com>
2025-05-12 08:58:17 -0300

    funções do modal

---
+++ Projetos/mini-projeto-javascript/script.js
@@ -0,0 +1,8 @@
+function novaTarefa() {
+    overlay.classList.add('active');
+    criarTarefa.classList.add('active');
+}
+function fecharModal() {
+    overlay.classList.remove('active');
+    criarTarefa.classList.remove('active');
+}
